918ba94b8225b74ad0d19b356185f61e756c1fc0
Ana Maria <109704779+Anawk@users.noreply.github.com>
2024-04-12 18:48:16 -0300

    Add files via upload

---
+++ testeAna/eventosTeste/Assets/js/es.js
@@ -0,0 +1,27 @@
+FullCalendar.globalLocales.push(function () {
+  'use strict';
+
+  var es = {
+    code: 'pt-br',
+    week: {
+      dow: 1, // Monday is the first day of the week.
+      doy: 4, // The week that contains Jan 4th is the first week of the year.
+    },
+    buttonText: {
+      prev: 'Anterior',
+      next: 'Seguinte',
+      today: 'Hoje',
+      month: 'Mes',
+      week: 'Semana',
+      day: 'Dia',
+      list: 'Agenda',
+    },
+    weekText: 'Sm',
+    allDayText: 'Dias',
+    moreLinkText: 'más',
+    noEventsText: 'Sem texto de eventos',
+  };
+
+  return es;
+
+}());
